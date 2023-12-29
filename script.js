82dec2a9e5bc4146552cad56b26d99427a2c3870
rupesh8690 <rupeshlove8690@gmail.com>
2023-12-29 19:30:52 +0545

    completed the assignment

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ let newH=document.createElement('h3');
 newH.innerText="I'm a blue h3!";
 newH.classList.add('myheading');
 document.querySelector('body').append(newH);
+
+let newD=document.createElement('div');
+newD.classList.add("mybox");
+document.querySelector('body').append(newD);
+
+let newH1=document.createElement('h1');
+newH1.innerText="I'm in a div";
+newD.appendChild(newH1);
+
+let p=document.createElement('p');
+p.innerText="MET TOO!!";
+newD.appendChild(p);
